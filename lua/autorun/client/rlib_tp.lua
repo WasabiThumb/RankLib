ccd91e02b88c6a4f727a7f2d899ba4096f9e8ce9
Wasabi_Thumbs <wasabithumbs@gmail.com>
2017-07-20 12:52:01 -0400

    Update rlib_tp.lua

--- lua/autorun/client/rlib_tp.lua
+++ lua/autorun/client/rlib_tp.lua
@@ -1,12 +1,16 @@
 local tprsubject;
+local istorequester = false;
 
 net.Receive( "tpr_receive", function()
     tprsubject = net.ReadEntity()
+    istorequester = net.ReadBool()
 end )
 
 net.Receive( "tpra", function()
-    if tpsubject:IsValid() then
+    if tpsubject:IsValid() and !istorequester then
       LocalPlayer():SetPos( tprsubject:GetPos() )
+    else if tpsubject:IsValid() and istorequester then
+      tprsubject:SetPos( LocalPlayer():GetPos() )
     else
       chat.AddText( "Player is offline!" )
     end
